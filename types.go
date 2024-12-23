ffc7371c5c52b43d2f244d1e7d5a4670c1819fd0
Purusothaman Srinivasan <60213224+purusothamanns@users.noreply.github.com>
2024-12-23 18:19:56 +0530

    Update types.go

--- types.go
+++ types.go
@@ -2,17 +2,12 @@
 package main
 
 // FakeReport contains OS, Arch, and Package information
-type FakeReport struct {
-	OSType    string
-	OSVersion string
-	Arch      string
-	Packages  []FakePackage
+type VulnerabilityReport struct {
+	basic_plan_component_vulnerability_fixes  [] Vulnerability
 }
 
 // FakePackage contains package and vulnerability information
-type FakePackage struct {
-	Name             string
-	InstalledVersion string
-	FixedVersion     string
-	VulnerabilityID  string
+type Vulnerability struct {
+	current_component_purl string
+	target_component_purl string
 }
